1ad7e553085425645f29c8aec666a5134857732e
Marius Obert <marius.obert@sap.com>
2019-12-03 11:17:06 +0100

    Solve day 3 part 1

---
+++ 03/wires.js
@@ -0,0 +1,86 @@
+const testInput = [{
+    wire1: ['R8', 'U5', 'L5', 'D3'],
+    wire2: ['U7', 'R6', 'D4', 'L4']
+}, {
+    wire1: ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+    wire2: ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
+}, {
+    wire1: ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
+    wire2: ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
+}];
+const input = require('./input');
+
+function addVector(a, b) { //From https://stackoverflow.com/questions/7135874/element-wise-operations-in-javascript
+    return a.map((e, i) => e + b[i]);
+}
+
+function absoluteSum(akku, num) {
+    if (num < 0) {
+        num = -num;
+    };
+    return akku + num
+}
+
+function min(min, num) {
+    min = min || num;
+    return min > num ? num : min;
+}
+
+function convertSegment(segment) {
+    let parsed = {
+        length: +segment.slice(1)
+    }
+    switch (true) {
+        case segment.indexOf("R") === 0:
+            parsed.vector = [0, 1];
+            return parsed;
+        case segment.indexOf("L") === 0:
+            parsed.vector = [0, -1];
+            return parsed;
+        case segment.indexOf("U") === 0:
+            parsed.vector = [1, 0];
+            return parsed;
+        case segment.indexOf("D") === 0:
+            parsed.vector = [-1, 0];
+            return parsed;
+            throw "Unknown segment: " + segment;
+    }
+}
+
+function calcVisitedNodes(route) {
+    let currectLocation = [0, 0];
+    let path = new Set([]);
+
+    route.map(convertSegment).forEach((move) => {
+        for (let step = 0; step < move.length; step++) {
+            currectLocation = addVector(currectLocation, move.vector);
+            path.add(currectLocation.join(',')); //save as string to allow easy comparison
+        }
+    });
+    return path;
+}
+
+function findIntersections(wire1, wire2) {
+    const path1 = calcVisitedNodes(wire1);
+    const path2 = calcVisitedNodes(wire2);
+    const intersections = new Set(
+        [...path1].filter(x => path2.has(x)));
+
+    return intersections;
+}
+
+function calcManhattenDist(input) {
+    return input.split(',')
+        .map((string) => +string)
+        .reduce(absoluteSum, 0)
+}
+
+
+function findClosestIntersection(wires) {
+    const intersections = findIntersections(wires.wire1, wires.wire2)
+    return [...intersections].map(calcManhattenDist).reduce(min);
+}
+
+
+console.log(testInput.map(findClosestIntersection));
+console.log(findClosestIntersection(input));
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # My solutions for AdventOfCode2019
 
 Check out <https://adventofcode.com/2019> if you don't know it yet.
+
+I'll do everything with JS, even though there might be more suited languages 😃
