50cd8a84ed3759fba396c176e5b52ce3aa2379d9
Marius Obert <marius.obert@sap.com>
2019-12-04 11:38:24 +0100

    solve day 4  part b

--- 04/seach.js
+++ 04/seach.js
@@ -6,7 +6,7 @@
 const MIN = 158126; //fullfils requirement 2
 const MAX = 624574;
 
-function getNextCandidate(num) { //fullfils requirement 3 and 4
+function getNextCandidateA(num) { //fullfils requirement 3 and 4
     const numAsString = (num + '');
     const dig1 = numAsString[0];
     const dig2 = numAsString[1];
@@ -39,14 +39,63 @@ function getNextCandidate(num) { //fullfils requirement 3 and 4
         dig5 === dig6;
 
     if (!adjacentsAreSame) {
-        return getNextCandidate(num + 1);
+        return getNextCandidateA(num + 1);
+    }
+    return num;
+}
+
+function getNextCandidateB(num) { //fullfils requirement 3 and 4
+    const numAsString = (num + '');
+    const dig1 = numAsString[0];
+    const dig2 = numAsString[1];
+    const dig3 = numAsString[2];
+    const dig4 = numAsString[3];
+    const dig5 = numAsString[4];
+    const dig6 = numAsString[5];
+
+    if (dig1 > dig2) {
+        return getNextCandidateB(+(dig1 + dig1 + dig1 + dig1 + dig1 + dig1))
+    }
+    if (dig2 > dig3) {
+        return getNextCandidateB(+(dig1 + dig2 + dig2 + dig2 + dig2 + dig2))
+    }
+    if (dig3 > dig4) {
+        return getNextCandidateB(+(dig1 + dig2 + dig3 + dig3 + dig3 + dig3))
+    }
+    if (dig4 > dig5) {
+        return getNextCandidateB(+(dig1 + dig2 + dig3 + dig4 + dig4 + dig4))
+    }
+    if (dig5 > dig6) {
+        return getNextCandidateB(+(dig1 + dig2 + dig3 + dig4 + dig5 + dig5))
+    }
+
+    let onlyTwoSame = false;
+    let previousAreSame = false;
+    let adjacentsAreSame = numAsString[0] === numAsString[1];
+
+    for (let idx = 0; idx < 5; idx++) {
+        let digit1 = numAsString[idx];
+        let digit2 = numAsString[idx + 1];
+        let digit3 = numAsString[idx + 2];
+
+        let nextAreSame = digit2 === digit3;
+        onlyTwoSame = onlyTwoSame ||
+            !previousAreSame && adjacentsAreSame && !nextAreSame
+
+        previousAreSame = adjacentsAreSame;
+        adjacentsAreSame = nextAreSame;
+    }
+
+
+    if (!onlyTwoSame) {
+        return getNextCandidateB(num + 1);
     }
     return num;
 }
 
 let candidates = []
 for (let number = MIN; number <= MAX;) { //fullfils requirement 1
-    let candidate = getNextCandidate(number);
+    let candidate = getNextCandidateB(number);
     if (candidate > MAX) {
         break;
     }
