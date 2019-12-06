3edc6f8a78f69597606d7e2b11d6af16e48d6b7a
Marius Obert <marius.obert@sap.com>
2019-12-06 17:16:31 +0100

    solve day 6 b

--- 06/graph.js
+++ 06/graph.js
@@ -1,16 +1,24 @@
+function constructRels(input) {
+    const outboundRels = {}, inboundRels = {};
+
+    for (let relationship of input) {
+        const [from, to] = relationship.split(')');
+
+        outboundRels[from] = outboundRels[from] || [];
+        outboundRels[to] = outboundRels[to] || [];
+        outboundRels[from].push(to);
+
+        inboundRels[to] = from;
+    }
+
+    return [outboundRels, inboundRels];
+}
+
 
 module.exports = {
+
     countRelationships: (input) => {
-        const outboundRels = {}, inboundRels = {};
-
-        for (let relationship of input) {
-            const [from, to] = relationship.split(')');
-            outboundRels[from] = outboundRels[from] || [];
-            outboundRels[to] = outboundRels[to] || [];
-            outboundRels[from].push(to);
-
-            inboundRels[to] = from;
-        }
+        const [outboundRels, inboundRels] = constructRels(input);
 
         function countRelationships(node) {
             const parent = inboundRels[node];
@@ -21,5 +29,26 @@ module.exports = {
         }
 
         return Object.keys(outboundRels).reduce((akku, node) => akku + countRelationships(node), 0);
+    },
+
+    calcDist: (input) => {
+
+        const [outboundRels, inboundRels] = constructRels(input);
+
+        function calcPath(node) {
+            const parent = inboundRels[node];
+            if (!parent) {
+                return new Set([]);
+            }
+            return calcPath(parent).add(node);
+        }
+
+        const jumpsSource2Root = calcPath(inboundRels['YOU']);
+        const jumpsDest2Root = calcPath(inboundRels['SAN']);
+
+        let intersection = new Set(
+            [...jumpsSource2Root].filter(x => jumpsDest2Root.has(x)));
+
+        return jumpsSource2Root.size + jumpsDest2Root.size - 2 * intersection.size;
     }
 };
